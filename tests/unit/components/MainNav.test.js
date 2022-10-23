68e59068a6ca29de1697228b5415a93e987ec243
Brendan Dagys <brendandagys@gmail.com>
2022-10-23 16:36:02 -0400

    Continue work in `MainNav` component
    Added a test to verify nav menu items.

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -5,6 +5,22 @@ import MainNav from "@/components/MainNav";
 describe("MainNav", () => {
   it("Displays company name", async () => {
     const wrapper = mount(MainNav);
-    expect(wrapper.text()).toMatch("Bobo Careers");
+    expect(wrapper.text()).toMatch("ABC Careers");
+  });
+
+  it("Displays menu items for navigation", () => {
+    const wrapper = mount(MainNav);
+    const navigationMenuItems = wrapper.findAll(
+      "[data-test='main-nav-list-item']"
+    );
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.text());
+    expect(navigationMenuTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at ABC",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
   });
 });
