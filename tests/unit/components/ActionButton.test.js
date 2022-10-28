10f0e4f69deeb88400a4f99c8a158fc402ab03d5
Brendan Dagys <brendandagys@gmail.com>
2022-10-27 20:57:01 -0400

    More work on sign-in button

---
+++ tests/unit/components/ActionButton.test.js
@@ -0,0 +1,28 @@
+import { mount } from "@vue/test-utils";
+
+import ActionButton from "@/components/ActionButton";
+
+describe("ActionButton", () => {
+  it("Renders text", () => {
+    const wrapper = mount(ActionButton, {
+      props: {
+        text: "I can be clicked",
+        type: "primary",
+      },
+    });
+
+    expect(wrapper.text()).toMatch("I can be clicked");
+  });
+
+  it("Applies one of several styles to button", () => {
+    const wrapper = mount(ActionButton, {
+      props: {
+        text: "I can be clicked",
+        type: "primary",
+      },
+    });
+
+    const button = wrapper.find("button");
+    expect(button.classes("primary")).toBe(true);
+  });
+});
