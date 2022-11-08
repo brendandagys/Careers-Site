a6e3324b3e462a2db66bb06f7bf103eebe7d5f99
Brendan Dagys <brendandagys@gmail.com>
2022-11-08 17:11:10 -0500

    More Vue Router updates

--- tests/unit/components/navigation/Subnav.test.js
+++ tests/unit/components/navigation/Subnav.test.js
@@ -3,44 +3,33 @@ import { mount } from "@vue/test-utils";
 import Subnav from "@/components/navigation/Subnav";
 
 describe("Subnav", () => {
+  const createConfig = (routeName) => ({
+    global: {
+      mocks: {
+        $route: {
+          name: routeName,
+        },
+      },
+    },
+    stubs: {
+      FontAwesomeIcon: true,
+    },
+  });
+
   describe("When user is on the job page", () => {
     it("Displays job count", () => {
-      const wrapper = mount(Subnav, {
-        global: {
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-        data() {
-          return {
-            onJobResultsPage: true,
-          };
-        },
-      });
-
+      const routeName = "JobResults";
+      const wrapper = mount(Subnav, createConfig(routeName));
       const jobCount = wrapper.find("[data-test='job-count']");
-
       expect(jobCount.exists()).toBe(true);
     });
   });
 
   describe("When user is not on the job page", () => {
     it("Does not display job count", () => {
-      const wrapper = mount(Subnav, {
-        global: {
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-        data() {
-          return {
-            onJobResultsPage: false,
-          };
-        },
-      });
-
+      const routeName = "Home";
+      const wrapper = mount(Subnav, createConfig(routeName));
       const jobCount = wrapper.find("[data-test='job-count']");
-
       expect(jobCount.exists()).toBe(false);
     });
   });
